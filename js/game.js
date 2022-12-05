bcb07f8f64a8cf88ee7e3430c3608bc77b79c624
Israel Menis <menismu@gmail.com>
2022-12-05 21:21:01 +0100

    Added enemy image and setup.

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 function preload() {
   this.load.image("sky", "assets/backgrounds/blue.png");
   this.load.image("player", "assets/characters/player.png");
+  this.load.image("enemy", "assets/characters/alien1.png");
 }
 
 function create() {
@@ -10,6 +11,12 @@ function create() {
   // playet setup
   const player = this.add.image(SCREEN_WIDTH / 2, SCREEN_HEIGHT, "player");
   player.setX((SCREEN_WIDTH - player.width * PLAYER_SCALE) / 2);
-  player.setY(SCREEN_HEIGHT - player.height * PLAYER_SCALE);
+  player.setY(SCREEN_HEIGHT - (player.height * PLAYER_SCALE) / 2);
   player.setScale(PLAYER_SCALE);
+
+  // enemy setup
+  const enemy = this.add.image(SCREEN_WIDTH / 2, SCREEN_HEIGHT, "enemy");
+  enemy.setX((SCREEN_WIDTH - enemy.width * ENEMY_SCALE) / 2);
+  enemy.setY((enemy.height * ENEMY_SCALE) / 2);
+  enemy.setScale(ENEMY_SCALE);
 }
