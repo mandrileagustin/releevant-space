e0300a1a1797dba86f4c216bbc285655dc54a25d
Israel Menis <menismu@gmail.com>
2022-12-05 21:06:52 +0100

    Added main character.

--- js/game.js
+++ js/game.js
@@ -1,7 +1,11 @@
 function preload() {
   this.load.image("sky", "assets/backgrounds/blue.png");
+  this.load.image("player", "assets/characters/player.png");
 }
 
 function create() {
   this.add.image(400, 300, "sky");
+
+  const player = this.add.image(400, 300, "player");
+  player.setScale(0.2);
 }
