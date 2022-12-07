5cab6ef9657270675940dbe48ac949db2ffca839
Agustin <mandrileagustin@hotmail.com>
2022-12-07 13:53:58 +0100

    delete bullets

--- js/game.js
+++ js/game.js
@@ -1,9 +1,15 @@
 /**
  * Variables used during the game.
  */
-let player;
-let enemy;
-let cursors;
+ let player;
+ let enemy;
+ let cursors;
+ let background1;
+ let background2;
+ let spaceBar;
+ let bullet=[];
+ let contBullet=0;
+ let frame=-1;
 
 /**
  * It prelaods all the assets required in the game.
@@ -41,6 +47,74 @@ function create() {
  * Updates each game object of the scene.
  */
 function update() {
+  this.add.ellipse(player.x,player.y-player.height/2*PLAYER_SCALE,180,200)
+  moverPlayer();
+  moverFondo();
+  if(frame<0){
+    disparar(this);
+  }
+  if (contBullet>0){
+  moverBala();
+  }
+  frame--;
+}
+
+function moverPlayer() {
   if (cursors.left.isDown) {
+    let xPlayer = player.x-PLAYER_VELOCITY;
+    if (xPlayer<(player.width/2)*PLAYER_SCALE){
+      xPlayer=(player.width/2)*PLAYER_SCALE;
+    }
+    player.setX(xPlayer);
+  }
+  else if(cursors.right.isDown){
+    let xPlayer = player.x+PLAYER_VELOCITY;
+    if (xPlayer>SCREEN_WIDTH-(player.width/2)*PLAYER_SCALE){
+      xPlayer=SCREEN_WIDTH-(player.width/2)*PLAYER_SCALE;
+    }
+    player.setX(xPlayer);
+  }
+  if(cursors.up.isDown){
+    let yPlayer = player.y-PLAYER_VELOCITY;
+    if (yPlayer<(player.height/2)*PLAYER_SCALE){
+      yPlayer=(player.height/2)*PLAYER_SCALE;
+    }
+    player.setY(yPlayer);
+  }
+  else if(cursors.down.isDown){
+    let yPlayer = player.y+PLAYER_VELOCITY;
+    if (yPlayer>SCREEN_HEIGHT-(player.height/2)*PLAYER_SCALE){
+      yPlayer=SCREEN_HEIGHT-(player.height/2)*PLAYER_SCALE;
+    }
+    player.setY(yPlayer);
   }
 }
+
+function moverFondo(){
+  background1.setY(background1.y+BACKGROUND_VELOCITY);
+  background2.setY(background2.y+BACKGROUND_VELOCITY);
+    
+    if (background1.y>background1.height+SCREEN_HEIGHT/2){
+      background1.setY(background2.y-background1.height)
+      let temp = background1;
+      background1 = background2;
+      background2 = temp;
+    }
+  }
+
+  function disparar(engine){
+    if(spaceBar.isDown){
+      bullet.push(engine.add.ellipse(player.x,player.y-player.height/2*PLAYER_SCALE, 5, 15, 0x6666ff))
+      contBullet++;
+      frame = 20;
+    }
+  }
+
+  function moverBala(){
+    for (let bala of bullet){
+      bala.setY(bala.y-BULLET_VELOCITY)
+      if( bala.y <0 - bala.height) {
+       bala.destroy();
+      }
+    }
+ }
