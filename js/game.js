79c5c3f33812669b9df6f310567b17db864736d5
Agustin <mandrileagustin@hotmail.com>
2022-12-08 19:17:10 +0100

    movimiento enemigo

--- js/game.js
+++ js/game.js
@@ -12,6 +12,10 @@
  let frame=-1;
  let score = 0;
  let scoreText;
+ let explosion;
+ let propulsion;
+ let disparo;
+ let pause = false;
  
  /**
   * It prelaods all the assets required in the game.
@@ -20,6 +24,11 @@
    this.load.image("sky", "assets/backgrounds/blue.png");
    this.load.image("player", "assets/characters/player.png");
    this.load.image("enemy", "assets/characters/alien2.png");
+   this.load.image("red","assets/backgrounds/red.png")
+   this.load.image("propulsion","assets/characters/propulsion.png")
+   this.load.audio("fondo","assets/sounds/fondo.mp3");
+   this.load.audio("disparo","assets/sounds/disparo.mp3")
+   this.load.image("gameOver","assets/backgrounds/gameOver.png")
  }
  
  /**
@@ -29,6 +38,15 @@
    // scene background
    background1 = this.add.image(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, "sky");
    background2 = this.add.image(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2-background1.height, "sky");
+
+   /////sonido
+
+  //  let sonidoFondo = this.sound.add("fondo");
+  //  sonidoFondo.loop = true;
+  //  sonidoFondo.play();
+
+   disparo = this.sound.add("disparo");
+   
  
    // playet setup
    player = this.add.image(SCREEN_WIDTH / 2, SCREEN_HEIGHT, "player");
@@ -41,6 +59,11 @@
    enemy.setX((SCREEN_WIDTH - enemy.width * ENEMY_SCALE) / 2);
    enemy.setY((enemy.height * ENEMY_SCALE) / 2);
    enemy.setScale(ENEMY_SCALE);
+
+   /// Game Over
+   gameOver = this.add.image(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, "gameOver");
+   gameOver.setX(SCREEN_WIDTH + gameOver.width);
+   gameOver.setY(SCREEN_HEIGHT + gameOver.height);
  
    //cursors map into game engine
    cursors = this.input.keyboard.createCursorKeys();
@@ -49,29 +72,58 @@
    spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
 
    scoreText = this.add.text(5, 5, "Score: 0", {
-    font: "32px Arial",
+    font: "12px Arial",
     fill: "#0095DD",
+ 
   });
 
+  // start = this.add.text( "START",{
+  //   font: "40px Arial",
+  //   fill: "#0095DD",
+  // })
+
+  explosion = this.add.particles("red").createEmitter({
+    sclale: {min: 0.5, max:0},
+    speed: {min:-100, max: 100},
+    quantity: 5,
+    frequency: 0.1,
+    lifespan: 200,
+    gravityY: 0,
+    on: false,
+  })
+
+  propulsion = this.add.particles("propulsion").createEmitter({
+    sclale: {min: 0.5, max:0},
+    speed: {min:-100, max: 100},
+    quantity: 6,
+    frequency: 0.1,
+    lifespan: 50,
+    gravityY: 0,
+    on: false,
+  })
  }
  
  /**
   * Updates each game object of the scene.
   */
- function update(){
+ function update()  {
+  if(pause) {
+    return;
+  }
    this.add.ellipse(player.x,player.y-player.height/2*PLAYER_SCALE,180,200)
    moverPlayer();
    moverFondo();
-   if(frame<0){
+   if(frame < 0){
      disparar(this);
    }
-   if (contBullet>0){
+   if (contBullet > 0){
    moverBala();
    }
    frame--;
-  
- }
- 
+   moverEnemy();
+   console.log(bullet.length);
+}
+///// movimiento player
  function moverPlayer() {
    if (cursors.left.isDown) {
      let xPlayer = player.x-PLAYER_VELOCITY;
@@ -101,9 +153,33 @@
      }
      player.setY(yPlayer);
    }
+   propulsion.setPosition(player.x , player.y +40);
+      propulsion.explode();
  }
- 
- function moverFondo(){
+/// movimiento enemigo
+ function moverEnemy()  {
+  enemy.setY(enemy.y + ENEMY_VELOCITY);
+  if (
+    player.x >= enemy.x - (enemy.width * ENEMY_SCALE) / 2 &&
+    player.x <= enemy.x + (enemy.width * ENEMY_SCALE) / 2 &&
+    player.y >= enemy.y - (enemy.height * ENEMY_SCALE) / 2 &&
+    player.y <= enemy.y + (enemy.height * ENEMY_SCALE) / 2 ||
+    enemy.y >= SCREEN_HEIGHT
+  ){
+
+    gameOver.setX(SCREEN_WIDTH /2)
+    gameOver.setY(SCREEN_HEIGHT /2)
+    enemy.destroy()
+    player.destroy();
+    explosion.setPosition(enemy.x, enemy.y);
+    explosion.explode();
+    pause = true;
+
+  }
+  
+ }
+/// movimiento fondo
+ function moverFondo()  {
    background1.setY(background1.y+BACKGROUND_VELOCITY);
    background2.setY(background2.y+BACKGROUND_VELOCITY);
      
@@ -114,9 +190,10 @@
        background2 = temp;
      }
    }
- 
-   function disparar(engine){
+///// disparos 
+   function disparar(engine)  {
      if(spaceBar.isDown){
+      disparo.play()
        bullet.push
        (engine.add.ellipse
         (player.x,player.y-player.height/2*PLAYER_SCALE, 5, 15, 0x6666ff))
@@ -124,29 +201,36 @@
        frame = 20;
      }
    }
- 
-   function moverBala(){
-     for (let bala of bullet){
-       bala.setY(bala.y-BULLET_VELOCITY)
-       if( bala.y <0 - bala.height) {
-        bala.destroy();
+//// movimiento bala 
+   function moverBala() {
+    let index =-1;
+     for (let i = 0; i < bullet.length ; i++){
+       bullet[i].setY(bullet[i].y-BULLET_VELOCITY)
+       if( bullet[i].y <0 - bullet[i].height) {
+        bullet[i].destroy();
+        index = i;
        }
-       colision(bala);
+       colision(bullet[i]);
+     }
+     if(index >=0 ){
+      bullet.splice(index,1)
      }
   }
-
-  function colision(bala){
+//// colision de objetos
+  function colision(bala) {
     if ((bala.x>=enemy.x-(enemy.width*ENEMY_SCALE)/2 && bala.x<=enemy.x+(enemy.width*ENEMY_SCALE)/2)&&
     (bala.y>=enemy.y-(enemy.height*ENEMY_SCALE)/2 && bala.y<=enemy.y+(enemy.height*ENEMY_SCALE)/2)){
       
       collectEnemy(bala, enemy)
+      explosion.setPosition(enemy.x, enemy.y);
+      explosion.explode();
+      enemy.setY((enemy.height * ENEMY_SCALE)/2); 
       enemy.setX(Math.floor(Math.random()*(SCREEN_WIDTH-enemy.width) + (enemy.width/2)));
       bala.destroy();
-      
     }
   }
-
-  function collectEnemy(){
+/////Score
+  function collectEnemy() {
     score += 1;
     scoreText.setText("Score:"+score)
   }
